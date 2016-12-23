94f1f0988c7f55911fca05465860dba5736d4fc3
jpablofarias <jpablo.farias@trademedia.cl>
2016-12-23 17:49:42 -0300

    capitulo 2 de expresiones regulares

--- docu_js/js/js/regular_expressions.js
+++ docu_js/js/js/regular_expressions.js
@@ -1,5 +1,7 @@
 /*************************************
+
 	REGULAR EXPRESIONS
+
 **************************************
 
 
@@ -21,9 +23,11 @@ PATTERN FLAGS
 
 Existen 3 metodos.
 
-ignore - i
-multiline - multiline
-global - g
+i : ignore			Ignora el case sensitive	
+
+m : multiline		Recoge los matchs tratados como lineas independientes
+
+g : global			Recoge todos los matchs posibles
 
 */
 
@@ -105,7 +109,7 @@ var str ="123andfour"
 str.match(/1\d3/g) // ["123"] Porque \d matchea cualquier digito entre el "1" y el "3"
 
 
-\w digit
+\w character
 
 var str ="sk8ter"
 str.match(/k\wt/g);// ["k8t"] Cualquier underscore, letra, numero, upper o lowercase
@@ -119,12 +123,14 @@ var str ="123andfour"
 str.match(/o\Dr/g) //["our"] Cualquier elemento que NO sea numero, en este caso "u".
 
 
-\W digit
+\W character
 var str ="sk*ter"
 str.match(/k\Wt/g);//["k*t"] Cualquier cosa que no sea ni letra ni numero, en este caso, *
 
 
-//Defining ranges in Regex
+
+
+//Defining rangos in Regex
 
 var str ="bicycle"
 
@@ -146,10 +152,128 @@ str.match(/Hello[\- ]world/); // ["Hello world"]
 
 //Negated ranges
 
-var str ="sk8ter"
+var str ="sK8ter"
 
-str.match(/[^a-z]/gi); //["8"] Cualquier elemento que NO sea del alfabeto
+str.match(/[^a-z]/gi); //["8"] Cualquier elemento que NO sea del alfabeto. La letra "i" sirve para ignorar el case sensitive. D
+
+str.match(/[^a-z]/g);//  ["K", "8"]  Si removemos la "i", la K mayuscula se escapa del patron, y tb es removida.
 
 
 /*************
   Multipliers
+
+
+Existen 3 multipliers in Regex:
+
++ : This matches one or more occurrences
+? : This matches zero or one occurrence
+* : This matches zero or more occurrences*/
+
+
+
+// + signo. 
+//Identifica que el pattern puede aparecer una o más veces
+
+var str ="Timothy sam Robert"
+
+str.match(/[A-Z][a-z]+/g);		//	["Timothy", "Robert"]. Acá estoy pidiendo una alta y al menos una baja, o más.
+
+str.match(/[A-Z][a-z][a-z]/g);	//	["Tim", "Rob"].  Acá estoy pidiendo Una alta y 2 bajas, nada más que eso
+
+
+
+//? Signo.  
+//El caracter predecesor del signo puede aparecer una o ninguna vez (en este caso la "S")
+
+var str="apple Apples Applse"
+
+str.match(/apples?/gi);	//["apple", "Apples"]
+
+
+
+//* signo Matching zero o más 
+// El pattern puede repetirse, y puede contener el "!" como no contenerlo
+
+var str="update update! updatess!! update!!!!"
+
+str.match(/update!*/g);	//["update", "update!", "update", "update!!!!"]
+
+
+
+/*-------------------
+Custon quantifiers
+----------------------
+
+//	{3}... Definir un numero determinado de repeticiones*/
+
+
+var str = "760-277-11"
+
+str.match(/\d{3}-\d{3}-\d{2}/);// ["760-277-11"] Porque pido 3 digitos, guion, 3 digitos, guion y 2 digitos
+
+
+
+//	{3,}	... Definir determinadas repeticiones o más
+
+
+var str="passwordSecret"
+
+str.match(/.{6,}/);//["passwordSecret"]. 	Porque tiene 6 o más carácteres.
+
+
+// {3,10}   ... Capturar entre un minimo y un máximo.
+
+
+var str="103510351035" //length de 12
+
+str.match(/.{10,15}/);//	["103510351035"] Porque tiene más del minimo y menos del máximo
+
+str.match(/.{15,20}/);//null   Porque no tiene el mínimo.
+
+
+// yes | no   ...Captura opciones alternas
+
+var str= "yes no dont nop"
+
+str.match(/yes|no/g); // ["yes", "no", "no"]
+
+
+
+//Creating a Regex for a telephone number
+
+
+var str = var str= "123-123-1234 (123)-123-1234 1231231234"
+
+str.match(/\(?\d{3}\)?-?\d{3}-?\d{4}/g); // ["123-123-1234", "(123)-123-1234", "1231231234"]
+
+
+
+/*Desgloce:
+
+/\(?\d{3}\)?-?\d{3}-?\d{4}/g
+
+
+		
+/	
+
+\(?			<-- Opcional contener un parentesis.
+
+
+\d{3}   	<-- Necesita matchear 3 digitos  
+
+\)?-?   	<-- Opcional contener parentesis de cierre o guion  
+
+
+\d{3}-? 	<-- Necesita 3 digitos, opcional guion 
+
+ \d{4}   	<-- Necesita 4 digitos
+
+ / g        <-- Global
+
+
+
+
+
+ **************
+ SPECIAL CHARACTERS
+
