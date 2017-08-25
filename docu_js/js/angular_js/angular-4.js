32f400dc665bcaaf5df68cdebf3ed1a2a43282f7
J. Pablo Farías <jpablo.farias@gmail.com>
2017-08-25 15:38:02 -0300

    update de info es6 y angular 4

---
+++ docu_js/js/angular_js/angular-4.js
@@ -0,0 +1,20 @@
+/* INSTALACION*/
+
+
+//Instalación de CLI
+npm install -g @angular/cli
+
+// Creo una carpeta, que puede ser cualquiera dado el caso
+ng new ng-animations 
+
+//Acceso a la carpeta
+cd ng-animations
+
+//Arrancar el servidor y mirar localhost
+ng serve
+
+
+
+
+/* NUEVO COMPONENTE*/
+ng generate component <name>  //  ng g c "name"
