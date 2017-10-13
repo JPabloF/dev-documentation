9f583749600c83203e6214383957609c8b1e0a97
J. Pablo Farías <jpablo.farias@gmail.com>
2017-10-13 16:13:38 -0300

    apuntes git

--- GIT.js
+++ GIT.js
@@ -1,129 +1,151 @@
-/* Como listar todas las ramas locales*/
+// GIT
 
-git branch -r
+ORIGIN --> Es el repositorio remoto
+MASTER --> Es el nombre de la rama
+
+origin/master // EJ: Nombre defecto de una rama remota master.
+
+
+Como listar todas las ramas locales
+---------------------------------------
+
+git branch // Lista solo las locales, marca con * la actual
+
+git branch -r // Lista remotas
 
 
 
+/////////////////////////////////////////////////////////////////////
 
-******************************************************
 					Working tree 
-******************************************************
 
 
+********************** LOCAL ***************************************
+
++*******************************************************************
 
 
-
-
-********************** LOCAL ******************************
-+**********************************************************
-
-
-
-
-		****** < WORKING DIRECTORY > ( Zona de de trabajo )
+		
+		-----------------------------------------------------------
+		1 ****** < WORKING DIRECTORY > ( Zona de de trabajo )
+		-----------------------------------------------------------
 
 			
 				
-			//	(+ --> Stage)	
+			//	( SUBIR A STAGE )	
 
-				$ git add <filename.ext> // > Pasar un archivo "preparado" a stage
-
-
-			//	(CTRL+Z -->  Working  <-- Stage  <-- History)
-
-				$ git checkout -- <filename.ext> // > Borro los cambios hechos en working, para recuperar la version stageada / commiteada
+				$ git add <filename.ext> // > Subir cambios en archivo a la zona de empaquetado para preparar un commit.
 
 
 
+			//	( BORRAR CAMBIOS &/o VOLVER A VERSION STAGEADA )
 
-		****** < STAGE/INDEX >  ( Zona de preparación y empaquetado)
-			
+				$ git checkout -- <filename.ext> // > Borro los cambios hechos en working, para VOLVER la version stageada / commiteada
 
 
-			//	(+  --> Head)
+
+		-----------------------------------------------------------		
+		2 ****** < STAGE/INDEX >  ( Zona de preparación y empaquetado )
+		-----------------------------------------------------------	
+
+
+			//	( COMMITEAR CAMBIOS )
 
 				$ git commit -m "message" // > Commiteo los cambios al head
 				$ git commit --amend	  // > Incluye mejoras bajo el mismo commit desde stage
 
 
-			//	(-  --> Stage --> working ) 
+			//	( ELIMINAR DE STAGE Y VOLVER AL ESTADO WORKING ) 
 
-				$ git reset HEAD <filename.ext>	// > Quito el archivo de stage sin borrar el working 	
-
-
+				$ git reset // > Quito el archivo de stage y me quedo con el working
 
 
-		****** < HEAD >  ( Versionado de paquetes commiteados)
 
+		-----------------------------------------------------------
+		3 ****** < HEAD >  ( COMMITS VERSIONADOS )
+		-----------------------------------------------------------
 
-			//	(+ --> REMOTE)
+			//	( SUBIR COMMIT A REMOTO )
 
 				$ git push // > Manda el commit al repo remoto
 				$ git push origin <rama>// > Manda el commit al repo remoto y rama específica
 
 
 
-			// ( - --> stagge)
-				$ git reset --soft HEAD~1	 //> Borra el último "commit" y lo devuelve a stage	
+			// ( ELIMINAR COMMIT  &  DEVOLVERLO A STAGE)
+
+				$ git reset --soft HEAD~1	 //> Borra el último "COMMIT" y lo devuelve a STAGGE, es como bajarlo de nivel	
 
 
 
-			// (+ --> Working)
-				$ git reset HEAD~1	//> (mixed) Borra el último "commit + el stage" pero mantiene los cambios de working	
+			// ( ELIMINAR COMMIT  &  DEVOLVER A STAGE  &  MANTENER EL WORKING INTACTO )
+
+				$ git reset HEAD~1	//> (mixed) Borra el último "COMMIT + STAGGE" pero mantiene los cambios del working directory	
 
 
 
-			// ( - --> WORKING)
-				$ git reset --hard HEAD~1  //> Borra todo este commit incluso los cambios del working
+			// ( ELIMINAR COMMIT  &  BORRAR EL WORKING DIRECTORY )
+
+				$ git reset --hard HEAD~1  //> Borra TODO este commit incluso los cambios del working
 
 
 
 ********************** REMOTE ******************************
+
 +***********************************************************
 
 
 			ORIGIN / RAMA
 
 
++*******************************************************************
 
-**********************************************************
+////////////////////////////////////////////////////////////////////
 
 
-//Agregar y commitear
+
+
+
+-------------------
+GESTION DE ACCIONES
+-------------------
+
+//COMMIT DIRECTO
+----------------
 
 $ git commit -a -m "MY MESSAGE HERE" //> -a significa all, y agrega archivos unstageados también
 
 
 
-ORIGIN --> ES el repositorio remoto
-MASTER --> Es el nombre de la rama
-
-
-
-//Guardar en stash
+// 	STASH & POP
+----------------
 $ git stash //> Me guarda los archivos y limpia el working 
-git stash pop //> Me devuelve las cosas a working
+$ git stash pop //> Me devuelve las cosas a working ...  * Requiere mergear
 
 
 
+// VER COMMITS 
+---------------
 $ git log origin/master..HEAD  //Me muestra mi ultimo commit local
 
 $ git diff origin/master..HEAD //ver diferencia
 
+$ git show <commit-id> // Detalle del ultimo commit
+
+$ git log --follow filename // Ver historia del archivo
+
 
 
 //IGNORAR ARCHIVOS LOCAL
+------------------------
 touch .gitignore
 
 
+//ELIMINAR ARCHIVOS
+-------------------
+
 $ git clean -n //> Me muestra que va a borrar
 $ git clean -f //> Quita los archivos no trackeados locales
-
-
-$ git show <commit number>
-
-
-$ git log --follow filename 
 
 
 
